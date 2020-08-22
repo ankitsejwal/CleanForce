f010f1c104e3065f8527b705d4c8375287660b34
ankitsejwal <ankit@sejw.al>
2020-08-22 17:30:58 +1000

    seperated routes

--- app.js
+++ app.js
@@ -1,43 +1,12 @@
 const express = require("express");
+const order = require("./routes/order");
+const home = require("./routes/home");
 
 const app = express();
 
-const stock = [
-  {
-    name: "lsjkdf",
-    quantity: 1,
-  },
-  {
-    name: "lsjfd",
-    quantity: 2,
-  },
-];
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-// get all stock orders
-app.get("/api/orders", (req, res) => {
-  res.send(stock);
-});
-
-// get a stock order
-app.get("/api/orders/:id", (req, res) => {
-  const id = req.params.id;
-});
-
-// post a stock order
-app.post("/api/orders", (req, res) => {
-  const order = {};
-});
-
-// update a stock order
-app.put("/api/orders/:id", (req, res) => {
-  const id = req.params.id;
-});
-// delete a stock order
-app.delete("/api/orders/:id", (res, req) => {});
+// middleware
+app.use("/", home);
+app.use("/api/orders", order);
 
 // webserver port
 app.listen("3000");
---
+++ routes/order.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const router = express.Router();
+
+const stock = [
+  {
+    name: "lsjkdf",
+    quantity: 1,
+  },
+  {
+    name: "lsjfd",
+    quantity: 2,
+  },
+];
+
+// get all stock orders
+router.get("/", (req, res) => {
+  res.send(stock);
+});
+
+// get a stock order
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+});
+
+// post a stock order
+router.post("/", (req, res) => {
+  const order = {};
+});
+
+// update a stock order
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+});
+// delete a stock order
+router.delete("/:id", (res, req) => {});
+
+module.exports = router;
